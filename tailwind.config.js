2c6bec4be4fdbcf013f69155f0bb153a7e2a88cd
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-14 14:35:00 +0800

    complete desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Almost_White: "hsl(0, 0%, 98%)",
+                c_Medium_Gray: "hsl(0, 0%, 41%)",
+                c_Almost_Black: "hsl(0, 0%, 8%)",
             },
             fontFamily: {
-
+                epilogue: ["Epilogue", "sans-serif"]
             }
         },
     },
